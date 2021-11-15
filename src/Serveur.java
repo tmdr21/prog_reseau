72cb3ce91266292bc43cfa0e2a96573dba10e2ec
Aicha101 <titagerita@gmail.com>
2021-11-15 10:59:26 +0000

    Serveur+Thread

---
+++ src/Serveur.java
@@ -0,0 +1,11 @@
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Serveur {
+    private String port;
+    private Socket listenSocket;
+    private ArrayList<Client> listClient;
+
+
+
+}
